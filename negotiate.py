9e788f0eeaee158c2995c7986f56b143fd78678c
krishsharan <krishsharan1998@gmail.com>
2019-07-10 00:08:12 +0530

    Create negotiate.py

---
+++ negotiate.py
@@ -0,0 +1,32 @@
+def negate_sequence(self,text):
+    """
+    Detects negations and transforms negated words into "not_" form.
+    """
+    negation = False
+    delims = "?.,!:;"
+    result = []
+#Here rather then applying split, we can directly feed our extracted symptoms list
+    words = text.split()
+    prev = None
+    pprev = None
+    for word in words:
+        # stripped = word.strip(delchars)
+        stripped = word.strip(delims).lower()
+        negated = "not_" + stripped if negation else stripped
+        result.append(negated)
+        if prev:
+            bigram = prev + " " + negated
+            result.append(bigram)
+            if pprev:
+                trigram = pprev + " " + bigram
+                result.append(trigram)
+            pprev = prev
+        prev = negated
+
+        if any(neg in word for neg in ["not", "n't", "no"]):
+            negation = not negation
+
+        if any(c in word for c in delims):
+            negation = False
+
+    return result
